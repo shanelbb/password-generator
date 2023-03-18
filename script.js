f63e455310ec7ef87cd201924ce69c650d70d4aa
Shanel Beebe <shabeebe1@WKMZTFD72BE9.global.publicisgroupe.net>
2023-03-17 19:24:36 -0600

    added logic to prevent user from submitting non-numerical characters.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+// Object to hold the prompt questions and answers
 const questions = [
   {
     question: "Choose a password length from 8 to 128 characters",
@@ -23,9 +24,12 @@ const questions = [
   },
 ];
 
+// query the button for the event listener to start generating the password
 const generateBtn = document.querySelector("#generate");
+// Variable to store the password criteria
 let criteria = '';
 
+// Function to loop through the questions object questions and add the characters the user wants to use to the criteria variable
 const chooseCriteria = () => {
   for (let i = 1; i < questions.length; i++) {
     let userChoice = confirm(questions[i].question)
@@ -33,22 +37,33 @@ const chooseCriteria = () => {
       criteria += questions[i].answer;
     }
   }
+
+  // Error handling for when user doesn't choose any character criteria
   if (!criteria) {
     alert("please choose at least one criteria");
+    // start function over
     chooseCriteria();
   } 
 }
 
+// Function to collect user answers to prompts and return the generated password
 const generatePassword = () => {
+  // variable to store the generated password and clears the returned password when user generates another password
   let returnedPassword = "";
+  // Variable with prompt for user to choose a password length between 8 and 28
   let pwLength = prompt(questions[0].question)
+  // Conditional statement to handle user input
+  // Function stops if user hits cancel on prompt
   if (!pwLength) {
     return ''
-  } else if (+pwLength < 8 || +pwLength > 128) {
+  }
+  // Error handling if user chooses number outside of 8 and 128
+  else if (+pwLength < 8 || +pwLength > 128 || isNaN(+pwLength)){
     alert("Please choose a number from 8 - 128");
     return generatePassword();
   } else {
-    chooseCriteria()
+    console.log(+pwLength);
+    chooseCriteria();
     let randomize;
     let char;
     for (let i = 0; i < +pwLength; i++) {
